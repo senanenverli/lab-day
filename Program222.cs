08266237f9b54eb4562987d46cbb7f9e43f99afe
unknown <senanaal@code.edu.az>
2023-11-03 22:01:17 +0400

    lab_tasks

---
+++ Program222.cs
@@ -0,0 +1,62 @@
+﻿//// bool AddNumbers(int a, int b)
+////{
+////    if (a == 30 || b == 30 || a + b == 30)
+////    {
+////        return true;
+////    }
+////    else
+////    {
+////        return false;       
+////    }
+////}
+////Console.WriteLine(AddNumbers(15 , 13));
+
+//using NCalc;
+//using System.Collections;
+
+////bool Numbers(int a)
+////{
+////    if (a % 3 ==0 || a % 7 == 0)
+////    {
+////        return true;
+////    }
+////    else
+////    {
+////        return false;
+////    }
+////}
+
+////int num = 21;
+////Console.WriteLine(Numbers(num));
+
+
+//int NearTo100(int num1, int num2)
+//{
+//    int diff1 = 100 - num1;
+//    int diff2 = 100 - num2;
+//    if (diff1 > diff2)
+//    {
+//        return num1;
+//    }
+//    else 
+//    {
+//        return num2;
+//    }
+//}
+//Console.WriteLine(NearTo100(24,25));
+////Task
+
+////int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+////int Task(int[] arr)
+////{
+////int sum = 0;
+////    for (int i = arr.Length - 1; i >= 0; i--)
+////    {
+////        if (i % 2 == 0)
+////        {
+////            sum += arr[i];
+////        }
+////    }
+////    return sum; 
+////}
+////Console.WriteLine(Task9);
